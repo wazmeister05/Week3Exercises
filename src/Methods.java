f97ca4485d2facce25031628f5ab330797a592e8
wazmeister05 <w.stolton@gmail.com>
2019-10-03 16:50:53 +0100

    Grade statistics

---
+++ src/Methods.java
@@ -0,0 +1,14 @@
+public class Methods {
+
+    public static void reverse(int[] array){
+
+    }
+
+    public static int[] copyOf(int[] array){
+
+    }
+
+    public static boolean swap(int[] array1, int[] array2){
+
+    }
+}
